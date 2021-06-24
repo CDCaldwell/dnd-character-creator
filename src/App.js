677a9e4fbc6717da376a032d62b838e0f01ee22d
ColinCaldwell <colindcaldwell@gmail.com>
2021-06-24 01:37:17 -0400

    added race choice and some basic styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
 
   const [classes, setClasses] = useState([])
   const [userClassChoice, setUserClassChoice] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [races, setRaces] = useState([])
+  const [userRaceChoice, setUserRaceChoice] = useState([])
 
   useEffect(() => {
     axios({
@@ -16,40 +17,64 @@ function App() {
       dataResponse: "json",
     }).then((res)=>{
       setClasses(res.data.results)
-      setIsLoading(false)
       });
     }, []);
 
+  useEffect(() => {
+    axios({
+      url:"https://www.dnd5eapi.co/api/races/",
+      method: "GET",
+      dataResponse: "json",
+    }).then((res)=>{
+      setRaces(res.data.results)
+    })
+  }, []);
     // as you add more dropdowns, add more arguments to this
-const generateSheet = (e, classChoice ) =>{
+const generateSheet = (e, classChoice, raceChoice ) =>{
   e.preventDefault();
-  // create copy of classes with a ... spread
   // const of userClassChoice and filter through classes and return classes.name === classChoice
   // create new state for user class choice, setUserClassChoice with the filtered results
-  const copyOfClassesData = [...classes]
+  const copyOfClassesData = [...classes];
+  const copyOfRacesData = [...races];
   const filteredClassChoice = copyOfClassesData.filter((characterClass =>{
     return characterClass.name === classChoice
   }))
   setUserClassChoice(filteredClassChoice)
+  const filteredRaceChoice = copyOfRacesData.filter((characterRace =>{
+    return characterRace.name === raceChoice
+  }))
+  setUserRaceChoice(filteredRaceChoice)
 }
-console.log(userClassChoice)
+
+
+
   return (
     <div className="App">
-      <UserForm
-      classes = {classes}
-      generateSheet ={generateSheet}
-      />
-            {/* add component for sheetGenerator
-                pass all filtered results from generateSheet function into props for sheetGenerator
-             */}
-      <div>
-        {userClassChoice != false ? <h2>{userClassChoice[0].name}</h2> : <></>}
-      </div>
+      <header>
+        <div className="wrapper">
+          <h1><span>D&D</span> Character Generator</h1>
+        </div>
+      </header>
+      <main>
+        <div className="wrapper">
+          <UserForm
+          classes = {classes}
+          races = {races}
+          generateSheet ={generateSheet}
+          />
+          <div className="results-div">
+            {userClassChoice!= false ? <h2>{userClassChoice[0].name}</h2> : <></>}
+            {userRaceChoice!= false ? <h2>{userRaceChoice[0].name}</h2> : <></>}
+          </div>
+        </div>
+        </main>
+        <footer>
+          <div className="wrapper">
+            <p>Created by Colin Caldwell at Juno College</p>
+          </div>
+        </footer>
     </div>
   );
 }
 
 export default App;
-
-
-// set state for 
--- src/UserForm.js
+++ src/UserForm.js
@@ -3,22 +3,37 @@ import { useState } from 'react';
 function UserForm(props){
 
     const [userClassChoice, setUserClassChoice] = useState('placeholder');
+    const [userRaceChoice, setUserRaceChoice] = useState('placeholder')
 
     const handleUserClassChoice = (e) => {
         setUserClassChoice(e.target.value)
     }
 
+    const handleUserRaceChoice = (e) => {
+        setUserRaceChoice(e.target.value)
+    }
+
     return(
             // as you add more dropdowns, add more arguments to this onSubmit function
-            <form className="dropdown-menus" onSubmit={(e) => {props.generateSheet(e, userClassChoice)}}>
-                <select name="class-choice" id="class-choice" value={userClassChoice} onChange={handleUserClassChoice}>
-                    <option value="placeholder" disabled>Pick a Class</option> 
-                    {props.classes.map((e) => {
-                        return(
-                            <option value={e.name} key={e.index}>{`${e.name}`}</option>
-                        )
-                    })}
-                </select>
+            <form className="generator-form" onSubmit={(e) => {props.generateSheet(e, userClassChoice, userRaceChoice)}}>
+                <div className="dropdown-menus">
+                    <select name="class-choice" id="class-choice" value={userClassChoice} onChange={handleUserClassChoice}>
+                        <option value="placeholder" disabled>Pick a Class</option> 
+                        {props.classes.map((e) => {
+                            return(
+                                <option value={e.name} key={e.index}>{`${e.name}`}</option>
+                            )
+                        })}
+                    </select>
+                    <select name="race-choice" id="race-choice" value={userRaceChoice} onChange={handleUserRaceChoice}>
+                        <option value="placeholder" disabled>Pick a Race</option> 
+                        {props.races.map((e) => {
+                            return(
+                                <option value={e.name} key={e.index}>{`${e.name}`}</option>
+                            )
+                        })}
+                    </select>
+                </div>
                 <button type="submit">Generate Character Sheet</button>    
             </form>
     )  
